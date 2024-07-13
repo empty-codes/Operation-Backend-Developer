7e6b59c1b70d4bc196a7942ebb10c86f51d2c25a
empty-codes <92234322+empty-codes@users.noreply.github.com>
2024-07-13 01:06:43 +0100

    Solution to Boss Battle 5 - The Password Validator

--- Phase 1 - Fundamentals of C#/Phase1-2-TheFivePrototypes/Program.cs
+++ Phase 1 - Fundamentals of C#/Phase1-2-TheFivePrototypes/Program.cs
@@ -1,93 +1,107 @@
-﻿// Boss Battle 1 - The Point
+﻿//// Boss Battle 1 - The Point
 
-Point point1 = new Point(2, 3);
-Point point2 = new Point(-4, 0);
+//Point point1 = new Point(2, 3);
+//Point point2 = new Point(-4, 0);
 
-Console.WriteLine($"({point1.X}, {point1.Y})");
-Console.WriteLine($"({point2.X}, {point2.Y})");
+//Console.WriteLine($"({point1.X}, {point1.Y})");
+//Console.WriteLine($"({point2.X}, {point2.Y})");
 
-// Boss Battle 2 - The Color
-Color color1 = new Color(245, 67, 50);
-Color color2 = Color.Purple;
+//// Boss Battle 2 - The Color
+//Color color1 = new Color(245, 67, 50);
+//Color color2 = Color.Purple;
 
-Console.WriteLine($"Color 1: ({color1.RedChannel}, {color1.GreenChannel}, {color1.BlueChannel})");
-Console.WriteLine($"Color 2: ({color2.RedChannel}, {color2.GreenChannel}, {color2.BlueChannel})");
+//Console.WriteLine($"Color 1: ({color1.RedChannel}, {color1.GreenChannel}, {color1.BlueChannel})");
+//Console.WriteLine($"Color 2: ({color2.RedChannel}, {color2.GreenChannel}, {color2.BlueChannel})");
 
-Console.Clear(); //comment this out to view the results for boss battle 1 and 2 when run
+//Console.Clear(); //comment this out to view the results for boss battle 1 and 2 when run
 
-// Boss Battle 3 - The Card
-void CreateCards()
-{
-    foreach(CardColor color in Enum.GetValues(typeof(CardColor)))
-    {
-        foreach(CardRank rank in Enum.GetValues(typeof(CardRank)))
-        {
-            Card card = new Card(color, rank);
-            Console.WriteLine($"The {color} {rank}");
-        }
-    }
-}
+//// Boss Battle 3 - The Card
+//void CreateCards()
+//{
+//    foreach(CardColor color in Enum.GetValues(typeof(CardColor)))
+//    {
+//        foreach(CardRank rank in Enum.GetValues(typeof(CardRank)))
+//        {
+//            Card card = new Card(color, rank);
+//            Console.WriteLine($"The {color} {rank}");
+//        }
+//    }
+//}
 
-CreateCards();
+//CreateCards();
 
-Console.Clear(); // comment this out to view the results for boss battle 3 when run
+//Console.Clear(); // comment this out to view the results for boss battle 3 when run
 
-// Boss Battle 4 - The Locked Door
- int SetPasscode() {
-    int passcode;
-    Console.WriteLine("You are to set a numeric passcode for the door, it must be a four digit number e.g 1234, 5678.");
-    while (true)
-    {
-        Console.Write("Enter a passcode for the door: ");
-        passcode = int.Parse(Console.ReadLine());
-        if (passcode >= 1000 && passcode <= 9999) break;
-    }
-    Console.Clear();
-    return passcode;
-}
+//// Boss Battle 4 - The Locked Door
+// int SetPasscode() {
+//    int passcode;
+//    Console.WriteLine("You are to set a numeric passcode for the door, it must be a four digit number e.g 1234, 5678.");
+//    while (true)
+//    {
+//        Console.Write("Enter a passcode for the door: ");
+//        passcode = int.Parse(Console.ReadLine());
+//        if (passcode >= 1000 && passcode <= 9999) break;
+//    }
+//    Console.Clear();
+//    return passcode;
+//}
 
-Door door = new Door(SetPasscode());
+//Door door = new Door(SetPasscode());
+
+//while(true)
+//{
+//    Console.WriteLine($"The door is {door.State}");
+//    Console.Write("Choose between 'open', 'close', 'lock', 'unlock', 'change passcode' and 'exit' commands: ");
+
+//    switch (Console.ReadLine())
+//    {
+//        case "open":
+//            door.OpenDoor();
+//            Console.Clear();
+//            break;
+//        case "close":
+//            door.CloseDoor();
+//            Console.Clear();
+//            break;
+//        case "lock":
+//            door.LockDoor();
+//            Console.Clear();
+//            break;
+//        case "unlock":
+//            Console.Write("Enter the current passcode: ");
+//            int unlockPasscode = int.Parse(Console.ReadLine());
+//            door.UnlockDoor(unlockPasscode);
+//            Console.Clear();
+//            break;
+//        case "change passcode":
+//            Console.Write("Enter the current passcode: ");
+//            int currentPasscode = int.Parse(Console.ReadLine());
+//            door.ChangePasscode(currentPasscode, SetPasscode());
+//            Console.Clear();
+//            break;
+//        case "exit":
+//            return;
+//        default:
+//            Console.WriteLine("Error! You can only choose between the existing commands.");
+//            Console.Clear();
+//            break;
+//    }
+//}
+
+// Boss Battle 5 - The Password Validator
 
 while(true)
 {
-    Console.WriteLine($"The door is {door.State}");
-    Console.Write("Choose between 'open', 'close', 'lock', 'unlock', 'change passcode' and 'exit' commands: ");
-
-    switch (Console.ReadLine())
-    {
-        case "open":
-            door.OpenDoor();
-            Console.Clear();
-            break;
-        case "close":
-            door.CloseDoor();
-            Console.Clear();
-            break;
-        case "lock":
-            door.LockDoor();
-            Console.Clear();
-            break;
-        case "unlock":
-            Console.Write("Enter the current passcode: ");
-            int unlockPasscode = int.Parse(Console.ReadLine());
-            door.UnlockDoor(unlockPasscode);
-            Console.Clear();
-            break;
-        case "change passcode":
-            Console.Write("Enter the current passcode: ");
-            int currentPasscode = int.Parse(Console.ReadLine());
-            door.ChangePasscode(currentPasscode, SetPasscode());
-            Console.Clear();
-            break;
-        case "exit":
-            return;
-        default:
-            Console.WriteLine("Error! You can only choose between the existing commands.");
-            Console.Clear();
-            break;
-    }
+    PasswordValidator user = new PasswordValidator(password);
+    Console.Write("Enter a password: ");
+    string? password = Console.ReadLine();
+    if (password == null) break;
+    if (user.isValid()) Console.WriteLine("This password is valid.");
+    else Console.WriteLine("This password is not allowed.");
 }
 
+
+// Classes
 
 // Boss Battle 1 - The Point
 public class Point {
@@ -246,6 +260,45 @@ public class Door
 
 }
 
+// Boss Battle 5 - The Password Validator
+class PasswordValidator
+{
+    public string Password { get; }
+
+    public PasswordValidator(string password)
+    {
+        Password = password;
+    }
+
+    public bool isValid()
+    {
+        int countLetters = 0, upperCases = 0, lowerCases = 0, digits = 0;
+        foreach (char letter in Password)
+        {
+            countLetters++;
+            if(char.IsUpper(letter) == true)
+            {
+                upperCases++;
+            }
+            if (char.IsLower(letter) == true)
+            {
+                lowerCases++;
+            }
+            if (char.IsDigit(letter) == true)
+            {
+                digits++;
+            }
+        }
+        if (countLetters >= 6 && countLetters <= 13 && upperCases >= 1 && lowerCases >= 1 && digits >= 1)
+        {
+            return true;
+        }
+        else return false;
+    }
+}
+
+
+// Enumerations
 
 // Boss Battle 3 - The Card
 enum CardColor { Red, Green, Blue, Yellow}
@@ -253,3 +306,6 @@ enum CardRank { One = 1, Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, D
 
 // Boss Battle 4 - The Locked Door
 public enum DoorState { Open, Closed, Locked}
+
+
+
